89f29d0039293f726b65c56e3c8791fa18dac35f
viveknanaware <viveknanaware95@gmail.com>
2022-03-18 12:17:30 +0530

    #Deployment changes for netlify hosting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
 { path: 'Contact', component: ContactComponent },
 { path: 'Home', component: HomeComponent },
-{ path: 'About', component: AboutusComponent }
+{ path: 'About', component: AboutusComponent },
+{ path: '', component: HomeComponent }
 ];
 
 @NgModule({
